5bb4c3d628c591856cf862b5d205631c31619612
Akana911Amboo <02230083.cst@rub.edu.bt>
2024-03-22 15:48:08 +0600

    Pattern is done

--- pattern.py
+++ pattern.py
@@ -1,3 +1,6 @@
 x = int(input("Enter the height of the triangle you want: "))
+print("Right Triangle Pattern: ")
 for i in range (1, x + 1):
-    print('*' * i)
+    for j in range (i):
+        print('*',end="" )
+    print()
